8f13e8d2a66d0975450a265b312d4174d09d993b
nonexee <45491571+nonexee@users.noreply.github.com>
2025-04-07 17:25:17 +0200

    Create screenshot.js

---
+++ api/screenshot.js
@@ -0,0 +1,81 @@
+const chrome = require('chrome-aws-lambda');
+let _page;
+
+async function getBrowser() {
+  console.log('-----------');
+  const executablePath = await chrome.executablePath;
+  console.log(executablePath);
+
+  // local development
+  if (!executablePath) {
+    const puppeteer = await import('puppeteer').then((m) => {
+      return m.default;
+    });
+
+    return puppeteer.launch({
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+  }
+
+  return chrome.puppeteer.launch({
+    args: chrome.args,
+    executablePath,
+    headless: true,
+  });
+}
+
+async function getPage() {
+  if (_page) return _page;
+
+  const browser = await getBrowser();
+  _page = await browser.newPage();
+  return _page;
+}
+
+function checkUrl(string) {
+  var url = '';
+  try {
+    url = new URL(string);
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+export async function getScreenshot(url, ratio = 1) {
+  const page = await getPage();
+  await page.goto(url, {
+    waitUntil: 'domcontentloaded',
+  });
+  await page.setViewport({
+    width: 1000,
+    height: 600,
+    devicePixelRatio: ratio,
+  });
+  const file = await page.screenshot();
+  return file;
+}
+
+module.exports = async (req, res) => {
+  if (!req.query.url) return res.status(400).send('No url query specified.');
+  if (!checkUrl(req.query.url))
+    return res.status(400).send('Invalid url query specified.');
+  try {
+    const file = await getScreenshot(req.query.url, req.query.ratio);
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader(
+      'Cache-Control',
+      'public, immutable, no-transform, s-maxage=604800, max-age=604800'
+    );
+    res.status(200).end(file);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        'The server encountered an error. You may have inputted an invalid query.'
+      );
+  }
+};
